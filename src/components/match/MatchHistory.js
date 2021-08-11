5d2eeaa988624c22bb8629a9f43780f8e82d09a2
rkdgnlgus2 <rkdgnlgus2@snu.ac.kr>
2021-08-11 19:25:13 +0900

    final test

--- src/components/match/MatchHistory.js
+++ src/components/match/MatchHistory.js
@@ -194,20 +194,21 @@ const rows = [
   createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
   createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
   createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-  createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
+  createData("Frozen yoghurt1", 159, 6.0, 24, 4.0, 3.99),
+  createData("Ice cream sandwich2", 237, 9.0, 37, 4.3, 4.99),
+  createData("<Eclai3></Eclai3>r", 262, 16.0, 24, 6.0, 3.79),
+  createData("Cupcake4", 305, 3.7, 67, 4.3, 2.5),
+  createData("Gingerbread5", 356, 16.0, 49, 3.9, 1.5),
 ];
 
 export default function MatchHistory({ matchInfo }) {
   useEffect(() => {
     if (!matchInfo) {
       console.log("props didn't come yet");
+    } else {
+      console.log("im in matchhistory");
+      console.log(matchInfo[0].info.gameMode);
     }
-    console.log("im in matchhistory");
-    console.log(matchInfo[0]);
   }, [matchInfo]);
   return (
     <TableContainer component={Paper}>
