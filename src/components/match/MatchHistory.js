ad211b19f07321f68311ddc38029a992c95eff90
rkdgnlgus2 <rkdgnlgus2@snu.ac.kr>
2021-08-19 14:57:35 +0900

    v37

--- src/components/match/MatchHistory.js
+++ src/components/match/MatchHistory.js
@@ -289,23 +289,11 @@ const matchBunch = (props) => {
   return row.map((match) => <Row match={match} />);
 };
 
-/*Row.propTypes = {
-  row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
-      PropTypes.shape({
-        amount: PropTypes.number.isRequired,
-        customerId: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
+Row.propTypes = {
+  match: PropTypes.shape({
+    championName: PropTypes.string.isRequired,
   }).isRequired,
-};*/
+};
 
 let rows = [{ championName: "Ryze" }];
 
