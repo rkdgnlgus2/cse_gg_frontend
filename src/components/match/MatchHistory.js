517da3901026c03edc1d0726011c463f97d54afc
rkdgnlgus2 <rkdgnlgus2@snu.ac.kr>
2021-08-25 16:34:54 +0900

    v12

--- src/components/match/MatchHistory.js
+++ src/components/match/MatchHistory.js
@@ -344,7 +344,7 @@ export default function MatchHistory({ summonerName }) {
           minute: "2-digit",
           second: "2-digit",
           hour12: false,
-        }).format(gameCreation + gameDuration),
+        }).format(gameCreation),
         "MM/DD/YYYY, hh:mm:ss a"
       ).fromNow();
 
